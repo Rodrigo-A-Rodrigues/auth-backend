532fadd48ae78eed64ae76fc840ea91c00ae3823
Rodrigo Rodrigues <rodrigorodrigues1807@gmail.com>
2021-05-31 19:46:12 -0300

    Mod of Admin

--- src/database.ts
+++ src/database.ts
@@ -6,7 +6,7 @@ export const users: UsersStore = new Map()
 export const tokens: RefreshTokensStore = new Map()
 
 export function seedUserStore() {
-  users.set('diego@rocketseat.team', {
+  users.set('rodrigo@rocketseat.team', {
     password: '123456',
     permissions: ['users.list', 'users.create', 'metrics.list'],
     roles: ['administrator']
